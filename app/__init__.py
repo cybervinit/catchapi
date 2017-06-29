d9fcc6e1159a675e05f8195a429aba064c7877ed
Vinit Soni <vinit@tdlab.io>
2017-06-28 22:07:06 -0400

    note to fix config error (not fixed yet)

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 api = Api(app)
 # app.config.from_object('config_app.ProductionConfig') 
 # app.config.from_pyfile('config.py')
+# FIX THE CONFIGURATION ERROR! 
 db.init_app(app)
 baseUrl = '/v1'
 
