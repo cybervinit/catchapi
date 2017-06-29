9659c154ca3da1402e2a9769a8c13a7114473941
Vinit Soni <vinit@tdlab.io>
2017-06-28 21:53:23 -0400

    remove import

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,6 @@ import os
 from flask import Flask, render_template, request
 from models import db
 from flask_restplus import Resource, Api, fields, reqparse
-import config
 
 
 app = Flask(__name__)
